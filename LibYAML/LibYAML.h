c847f4eb70b93f356e29051558811af5029e3bc4
Zhuhao Wang <zhuhaow@gmail.com>
2016-10-02 07:32:38 +0800

    Initial Commit

---
+++ LibYAML/LibYAML.h
@@ -0,0 +1,19 @@
+//
+//  LibYAML.h
+//  LibYAML
+//
+//  Created by Zhuhao Wang on 02/10/2016.
+//  Copyright © 2016 Zhuhao Wang. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for LibYAML.
+FOUNDATION_EXPORT double LibYAMLVersionNumber;
+
+//! Project version string for LibYAML.
+FOUNDATION_EXPORT const unsigned char LibYAMLVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LibYAML/PublicHeader.h>
+
+
